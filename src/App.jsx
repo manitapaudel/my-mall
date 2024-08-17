19fcf56937afdf0b269fcfafef465a3304e5e51f
Manita Paudel <paudel04manita@gmail.com>
2024-08-17 21:01:40 +0545

    Create routes, components folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <div className="text-red-600">Hi there!</div>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
