3e61d88d21e75d2a3d8f5fcbdef7b2e15801ecd7
Manita Paudel <paudel04manita@gmail.com>
2024-08-18 21:18:27 +0545

    Redux store and slices setup for products and cart

--- src/assets/mockData.js
+++ src/assets/mockData.js
@@ -1,3 +1,14 @@
+import Cap from "./images/cap.jpg";
+import Purse from "./images/purse.jpg";
+import Sandal from "./images/sandal.jpg";
+import Watch from "./images/watch.jpg";
+import Tshirt from "./images/t-shirt.jpg";
+import TruckToy from "./images/truck-toy.jpg";
+import Headphones from "./images/headphones.jpg";
+import Colors from "./images/colors.jpg";
+import Shampoos from "./images/shampoos.jpg";
+import Sunglasses from "./images/sunglasses.jpg";
+
 export const categories = [
   "Electronics",
   "Fashion",
@@ -5,4 +16,67 @@ export const categories = [
   "Beauty",
   "Sports",
   "Automative",
+];
+
+export const mockData = [
+  {
+    id: 1,
+    image: Cap,
+    name: "Product 1",
+    price: 29.99,
+  },
+  {
+    id: 2,
+    image: Purse,
+    name: "Product 2",
+    price: 46.99,
+  },
+  {
+    id: 3,
+    image: Sandal,
+    name: "Product 3",
+    price: 29.99,
+  },
+  {
+    id: 4,
+    image: Watch,
+    name: "Product 4",
+    price: 29.99,
+  },
+  {
+    id: 5,
+    image: Tshirt,
+    name: "Product 5",
+    price: 29.99,
+  },
+  {
+    id: 6,
+    image: TruckToy,
+    name: "Product 6",
+    price: 29.99,
+  },
+  {
+    id: 7,
+    image: Headphones,
+    name: "Product 7",
+    price: 29.99,
+  },
+  {
+    id: 8,
+    image: Shampoos,
+    name: "Product 8",
+    price: 29.99,
+  },
+  {
+    id: 9,
+    image: Sunglasses,
+    name: "Product 9",
+    price: 29.99,
+  },
+  {
+    id: 10,
+    image: Colors,
+    name: "Product 10",
+    price: 29.99,
+  },
 ];
