bb84f59d1ffe1a4af1567e77084882d423691ca2
Manita Paudel <paudel04manita@gmail.com>
2024-09-07 17:27:59 +0545

    Create login and register pages

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,43 @@
+const Login = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Login</h2>
+      <form>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            className="w-full px-3 py-2 border"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Password</label>
+          <input type="password" className="w-full px-3 py-2 border" />
+        </div>
+        <div className="mb-4 flex items-center justify-between">
+          <label className="inline-flex items-center">
+            <input type="checkbox" className="form-checkbox" />
+            <span className="ml-2 text-gray-700">Remember Me</span>
+          </label>
+          <a href="#" className="text-red-700">
+            Forgot Password
+          </a>
+        </div>
+        <div className="mb-4">
+          <button type="submit" className="w-full bg-red-600 text-white py-2">
+            Login
+          </button>
+        </div>
+      </form>
+      <div className="text-center">
+        <span className="text-gray-700">Don&apos;t have an account?</span>
+        <button className="text-red-800">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Register/index.jsx
@@ -0,0 +1,40 @@
+const Register = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+      <form>
+        <div className="mb-4">
+          <label className="block text-gray-700">Name</label>
+          <input
+            type="text"
+            className="w-full px-3 py-2 border"
+            placeholder="Enter name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Email</label>
+          <input
+            type="email"
+            className="w-full px-3 py-2 border"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Password</label>
+          <input type="password" className="w-full px-3 py-2 border" />
+        </div>
+        <div className="mb-4">
+          <button type="submit" className="w-full bg-red-600 text-white py-2">
+            Sign Up
+          </button>
+        </div>
+      </form>
+      <div className="text-center">
+        <span className="text-gray-700">Already have an account?</span>
+        <button className="text-red-800">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
